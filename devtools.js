f863ff50288971bb57d77187a2cba987797720b0
Enivia <aqingcc@outlook.com>
2021-08-25 22:15:09 +0800

    feat: simplify

--- devtools.js
+++ devtools.js
@@ -1,28 +1,5 @@
-// chrome.devtools.panels.create(
-//   'PreserveNetworkLog',
-//   '',
-//   '/panel.html',
-//   function (panel) {
-//     chrome.devtools.network.onRequestFinished.addListener(function (request) {
-//       chrome.storage.sync.get('enablePreserveRequestLog', (result) => {
-//         console.log(result.enablePreserveRequestLog);
-//         if (result.enablePreserveRequestLog) {
-//           request.getContent((content, encoding) => {
-//             console.log(content);
-//           });
-//         }
-//       });
-//     });
-//   }
-// );
-
 chrome.devtools.network.onRequestFinished.addListener(function (request) {
-  chrome.storage.sync.get('enablePreserveRequestLog', (result) => {
-    console.log(result.enablePreserveRequestLog);
-    if (result.enablePreserveRequestLog) {
-      request.getContent((content, encoding) => {
-        console.log(content);
-      });
-    }
+  request.getContent((content, encoding) => {
+    console.log(content);
   });
 });
